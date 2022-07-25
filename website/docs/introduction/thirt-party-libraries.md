b15303ebf9d3f4a444c058a15c11983fae8a73ca
Victor Franca <victor.pinheiro-franca@marsh.com>
2022-07-25 12:54:35 +0100

    organizing docs

--- website/docs/introduction/thirt-party-libraries.md
+++ website/docs/introduction/thirt-party-libraries.md
@@ -1,8 +1,8 @@
 ---
-sidebar_position: 0
+sidebar_position: 1
 ---
 
-# Tutorial Intro
+# Third-Party Libraries
 
 Let's discover **Docusaurus in less than 5 minutes**.
 
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'introduction/intro',
             position: 'left',
             label: 'Docs',
           },
@@ -83,10 +83,6 @@ const config = {
           {
             title: 'Docs',
             items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
               {
                 label: 'Solution Architecture',
                 to: '/docs/category/architecture',
