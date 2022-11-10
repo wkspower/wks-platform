8fb7758b7cb0ddd33ddf4a3cfa07d0aa5a3beb83
Victor Franca <victor@wkspower.com>
2022-11-10 11:20:16 +0000

    removing file after importing it

--- case-engine-rest-api/src/main/java/com/wks/caseengine/rest/app/DataImportCommandRunner.java
+++ case-engine-rest-api/src/main/java/com/wks/caseengine/rest/app/DataImportCommandRunner.java
@@ -1,11 +1,12 @@
 package com.wks.caseengine.rest.app;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -32,24 +33,24 @@ public class DataImportCommandRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Teste: " + dataImportFolder);
 		if (dataImportFolder != null && !dataImportFolder.isEmpty()) {
 
+			// load and import json files
 			Gson gson = new Gson();
-
 			listFiles(dataImportFolder).forEach(fileName -> {
 				JsonReader reader;
-				try {
-					reader = new JsonReader(new FileReader(fileName));
+				try (FileReader fileReader = new FileReader(fileName)) {
+					reader = new JsonReader(fileReader);
 					dataImportService.importData(gson.fromJson(reader, JsonObject.class));
-				} catch (FileNotFoundException e) {
+				} catch (IOException e1) {
 					// TODO Auto-generated catch block
-					e.printStackTrace();
+					e1.printStackTrace();
 				}
-
 			});
-		}
 
+			// delete files
+			Arrays.stream(new File(dataImportFolder).listFiles()).forEach(File::delete);
+		}
 	}
 
 	public Set<String> listFiles(String dir) throws IOException {
