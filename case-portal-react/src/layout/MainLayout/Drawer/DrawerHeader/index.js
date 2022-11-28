60d2963ec1511ec74cad839297f97919299b717c
Victor Franca <victor@wkspower.com>
2022-11-28 14:30:12 +0000

    disabling auto import

--- case-portal-react/src/layout/MainLayout/Drawer/DrawerHeader/index.js
+++ case-portal-react/src/layout/MainLayout/Drawer/DrawerHeader/index.js
@@ -18,15 +18,6 @@ const DrawerHeader = ({ open }) => {
         <DrawerHeaderStyled theme={theme} open={open}>
             <Stack direction="row" spacing={1} alignItems="center">
                 <Logo />
-                <Chip
-                    label={process.env.REACT_APP_VERSION}
-                    size="small"
-                    sx={{ height: 16, '& .MuiChip-label': { fontSize: '0.625rem', py: 0.25 } }}
-                    component="a"
-                    href="https://github.com/codedthemes/mantis-free-react-admin-template"
-                    target="_blank"
-                    clickable
-                />
             </Stack>
         </DrawerHeaderStyled>
     );
