3f2d0e59b509fccb02221fa3227a188585d844e1
francav <victor@wkspower.com>
2023-06-08 10:00:50 +0100

    bpmn web modeler

--- case-portal-react/src/views/management/processDef/bpmnModeler.js
+++ case-portal-react/src/views/management/processDef/bpmnModeler.js
@@ -1,159 +1,158 @@
-// import CloseIcon from '@mui/icons-material/Close';
-// import AppBar from '@mui/material/AppBar';
-// import Button from '@mui/material/Button';
-// import Dialog from '@mui/material/Dialog';
-// import IconButton from '@mui/material/IconButton';
-// import Slide from '@mui/material/Slide';
-// import Toolbar from '@mui/material/Toolbar';
-// import Typography from '@mui/material/Typography';
-// import React, { useCallback, useMemo, useRef, useState, useEffect } from 'react';
-import React from 'react';
-// import { ProcessDefService } from 'services/ProcessDefService';
-// import { DeploymentService } from 'services/DeploymentService';
+import CloseIcon from '@mui/icons-material/Close';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import IconButton from '@mui/material/IconButton';
+import Slide from '@mui/material/Slide';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import React, { useCallback, useMemo, useRef, useState, useEffect } from 'react';
+import { ProcessDefService } from 'services/ProcessDefService';
+import { DeploymentService } from 'services/DeploymentService';
 
-// import {
-//     BpmnModeler as CamundaWebModeler,
-//     isBpmnIoEvent,
-//     isContentSavedEvent,
-//     isNotificationEvent,
-//     isPropertiesPanelResizedEvent,
-//     isUIUpdateRequiredEvent
-// } from "@miragon/camunda-web-modeler";
+import {
+    BpmnModeler as CamundaWebModeler,
+    isBpmnIoEvent,
+    isContentSavedEvent,
+    isNotificationEvent,
+    isPropertiesPanelResizedEvent,
+    isUIUpdateRequiredEvent
+} from "@miragon/camunda-web-modeler";
 
-// const Transition = React.forwardRef(function Transition(props, ref) {
-//     return <Slide direction="up" ref={ref} {...props} />;
-// });
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export const BPMNModeler = ({ open, keycloak, processDef, handleClose }) => {
 
-    // const [bpmnXml, setBpmnXml] = useState();
+    const [bpmnXml, setBpmnXml] = useState();
 
-    // useEffect(() => {
-    //     ProcessDefService.getBPMNXml(keycloak, processDef.id)
-    //         .then((data) => {
-    //             setBpmnXml(data);
-    //         })
-    //         .catch((err) => {
-    //             setBpmnXml(null);
-    //             console.log(err.message);
-    //         });
-    // }, [open]);
+    useEffect(() => {
+        ProcessDefService.getBPMNXml(keycloak, processDef.id)
+            .then((data) => {
+                setBpmnXml(data);
+            })
+            .catch((err) => {
+                setBpmnXml(null);
+                console.log(err.message);
+            });
+    }, [open]);
 
-    // const modelerRef = useRef();
+    const modelerRef = useRef();
 
-    // const onXmlChanged = useCallback((
-    //     newXml,
-    //     newSvg,
-    //     reason
-    // ) => {
-    //     // console.log(`Model has been changed because of ${reason}`);
-    //     // Do whatever you want here, save the XML and SVG in the backend etc.
-    //     setBpmnXml(newXml);
-    // }, []);
+    const onXmlChanged = useCallback((
+        newXml,
+        newSvg,
+        reason
+    ) => {
+        // console.log(`Model has been changed because of ${reason}`);
+        // Do whatever you want here, save the XML and SVG in the backend etc.
+        setBpmnXml(newXml);
+    }, []);
 
-    // const onSaveClicked = useCallback(async () => {
-    //     if (!modelerRef.current) {
-    //         // Should actually never happen, but required for type safety
-    //         return;
-    //     }
+    const onSaveClicked = useCallback(async () => {
+        if (!modelerRef.current) {
+            // Should actually never happen, but required for type safety
+            return;
+        }
 
-    //     // console.log("Saving model...");
+        // console.log("Saving model...");
 
-    //     const result = await modelerRef.current.save();
-    //     DeploymentService.deploy(keycloak, result.xml).then(() => {
-    //         handleClose();
-    //     });
+        const result = await modelerRef.current.save();
+        DeploymentService.deploy(keycloak, result.xml).then(() => {
+            handleClose();
+        });
 
-    //     // console.log("Saved model!", result.xml, result.svg);
-    // }, []);
+        // console.log("Saved model!", result.xml, result.svg);
+    }, []);
 
-    // const onEvent = useCallback(async (event) => {
-    //     if (isContentSavedEvent(event)) {
-    //         // Content has been saved, e.g. because user edited the model or because he switched
-    //         // from BPMN to XML.
-    //         onXmlChanged(event.data.xml, event.data.svg, event.data.reason);
-    //         return;
-    //     }
+    const onEvent = useCallback(async (event) => {
+        if (isContentSavedEvent(event)) {
+            // Content has been saved, e.g. because user edited the model or because he switched
+            // from BPMN to XML.
+            onXmlChanged(event.data.xml, event.data.svg, event.data.reason);
+            return;
+        }
 
-    //     if (isNotificationEvent(event)) {
-    //         // There's a notification the user is supposed to see, e.g. the model could not be
-    //         // imported because it was invalid.
-    //         return;
-    //     }
+        if (isNotificationEvent(event)) {
+            // There's a notification the user is supposed to see, e.g. the model could not be
+            // imported because it was invalid.
+            return;
+        }
 
-    //     if (isUIUpdateRequiredEvent(event)) {
-    //         // Something in the modeler has changed and the UI (e.g. menu) should be updated.
-    //         // This happens when the user selects an element, for example.
-    //         return;
-    //     }
+        if (isUIUpdateRequiredEvent(event)) {
+            // Something in the modeler has changed and the UI (e.g. menu) should be updated.
+            // This happens when the user selects an element, for example.
+            return;
+        }
 
-    //     if (isPropertiesPanelResizedEvent(event)) {
-    //         // The user has resized the properties panel. You can save this value e.g. in local
-    //         // storage to restore it on next load and pass it as initializing option.
-    //         // console.log(`Properties panel has been resized to ${event.data.width}`);
-    //         return;
-    //     }
+        if (isPropertiesPanelResizedEvent(event)) {
+            // The user has resized the properties panel. You can save this value e.g. in local
+            // storage to restore it on next load and pass it as initializing option.
+            // console.log(`Properties panel has been resized to ${event.data.width}`);
+            return;
+        }
 
-    //     if (isBpmnIoEvent(event)) {
-    //         // Just a regular bpmn-js event - actually lots of them
-    //         return;
-    //     }
+        if (isBpmnIoEvent(event)) {
+            // Just a regular bpmn-js event - actually lots of them
+            return;
+        }
 
-    //     // eslint-disable-next-line no-console
-    //     // console.log("Unhandled event received", event);
-    // }, [onXmlChanged]);
+        // eslint-disable-next-line no-console
+        // console.log("Unhandled event received", event);
+    }, [onXmlChanged]);
 
-    // /**
-    //  * ====
-    //  * CAUTION: Using useMemo() is important to prevent additional render cycles!
-    //  * ====
-    //  */
+    /**
+     * ====
+     * CAUTION: Using useMemo() is important to prevent additional render cycles!
+     * ====
+     */
 
-    // const xmlTabOptions = useMemo(() => ({
-    //     className: undefined,
-    //     disabled: undefined,
-    //     monacoOptions: undefined
-    // }), []);
+    const xmlTabOptions = useMemo(() => ({
+        className: undefined,
+        disabled: undefined,
+        monacoOptions: undefined
+    }), []);
 
-    // const propertiesPanelOptions = useMemo(() => ({
-    //     className: undefined,
-    //     containerId: undefined,
-    //     container: undefined,
-    //     elementTemplates: undefined,
-    //     hidden: undefined,
-    //     size: {
-    //         max: undefined,
-    //         min: undefined,
-    //         initial: undefined
-    //     }
-    // }), []);
+    const propertiesPanelOptions = useMemo(() => ({
+        className: undefined,
+        containerId: undefined,
+        container: undefined,
+        elementTemplates: undefined,
+        hidden: undefined,
+        size: {
+            max: undefined,
+            min: undefined,
+            initial: undefined
+        }
+    }), []);
 
-    // const modelerOptions = useMemo(() => ({
-    //     className: undefined,
-    //     refs: [modelerRef],
-    //     container: undefined,
-    //     containerId: undefined,
-    //     size: {
-    //         max: undefined,
-    //         min: undefined,
-    //         initial: undefined
-    //     }
-    // }), []);
+    const modelerOptions = useMemo(() => ({
+        className: undefined,
+        refs: [modelerRef],
+        container: undefined,
+        containerId: undefined,
+        size: {
+            max: undefined,
+            min: undefined,
+            initial: undefined
+        }
+    }), []);
 
-    // const bpmnJsOptions = useMemo(() => undefined, []);
+    const bpmnJsOptions = useMemo(() => undefined, []);
 
-    // const modelerTabOptions = useMemo(() => ({
-    //     className: undefined,
-    //     disabled: undefined,
-    //     bpmnJsOptions: bpmnJsOptions,
-    //     modelerOptions: modelerOptions,
-    //     propertiesPanelOptions: propertiesPanelOptions
-    // }), [bpmnJsOptions, modelerOptions, propertiesPanelOptions]);
+    const modelerTabOptions = useMemo(() => ({
+        className: undefined,
+        disabled: undefined,
+        bpmnJsOptions: bpmnJsOptions,
+        modelerOptions: modelerOptions,
+        propertiesPanelOptions: propertiesPanelOptions
+    }), [bpmnJsOptions, modelerOptions, propertiesPanelOptions]);
 
 
     return (
         <div>
-            {/* <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+            <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
                 <AppBar sx={{ position: 'relative' }}>
                     <Toolbar>
                         <IconButton
@@ -182,7 +181,7 @@ export const BPMNModeler = ({ open, keycloak, processDef, handleClose }) => {
                         modelerTabOptions={modelerTabOptions} />
                 </div>
 
-            </Dialog> */}
+            </Dialog>
         </div>
     );
 };
