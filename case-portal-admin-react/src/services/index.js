50420c8eb3f4f9f4b4d951596a8661abe91f26ad
francav <victor@wkspower.com>
2023-06-30 14:45:09 +0100

    removing case-portal-admin-react

--- case-portal-admin-react/src/services/index.js
+++
@@ -1,7 +0,0 @@
-import { BpmService } from './BpmService';
-import { CaseDefService } from './CaseDefService';
-import { ProcessDefService } from './ProcessDefService';
-import { FormService } from './FormService';
-import { RecordTypeService } from './RecordTypeService';
-
-export { BpmService, CaseDefService, ProcessDefService, FormService, RecordTypeService };
