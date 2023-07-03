a84fb41061e472e89e14bf5e552acafa8208bfb2
francav <victor@wkspower.com>
2023-07-03 15:33:21 +0100

    adding copyright

--- case-engine/src/main/java/com/wks/caseengine/cases/definition/CaseDefinition.java
+++ case-engine/src/main/java/com/wks/caseengine/cases/definition/CaseDefinition.java
@@ -1,3 +1,14 @@
+/*
+ * WKS Platform - Open-Source Project
+ * 
+ * This file is part of the WKS Platform, an open-source project developed by WKS Power.
+ * 
+ * WKS Platform is licensed under the MIT License.
+ * 
+ * © 2021 WKS Power. All rights reserved.
+ * 
+ * For licensing information, see the LICENSE file in the root directory of the project.
+ */
 package com.wks.caseengine.cases.definition;
 
 import java.util.ArrayList;
