fbd43a8ef8d5bfe27f5b215dc1286de5c2673508
francav <victor@wkspower.com>
2024-03-01 17:37:50 +0000

    adding novu-app-id request into react portal

--- apps/react/case-portal/src/consts/index.js
+++ apps/react/case-portal/src/consts/index.js
@@ -8,7 +8,25 @@ const Config = {
     WebsocketsTopicCaseCreated: process.env.REACT_APP_WEBSOCKETS_CASE_CREATED,
     WebsocketsTopicHumanTaskCreated: process.env.REACT_APP_WEBSOCKETS_HUMAN_TASK_CREATED,
     NovuEnabled: process.env.REACT_APP_NOVU_ENABLED === 'true' || false,
-    NovuAppId: process.env.REACT_APP_NOVU_APP_ID,
+    NovuPublisherApiUrl: process.env.REACT_APP_NOVU_PUBLISHER_API_URL,
+    NovuAppId: process.env.REACT_APP_NOVU_ENABLED === 'true' ? await fetchNovuAppId() : undefined,
 };
 
+async function fetchNovuAppId() {
+    try {
+        const apiUrl = `${process.env.REACT_APP_NOVU_PUBLISHER_API_URL}/novu-app-id`;
+
+        const response = await fetch(apiUrl);
+        
+        if (!response.ok) {
+            throw new Error(`Failed to fetch NovuAppId. Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data.novuAppId;
+    } catch (error) {
+        console.error('Error fetching NovuAppId:', error);
+        return null;
+    }
+}
 export default Config;
