f50ce2ee2a35ba69b242e0a2283b620312aa88d7
francav <victor@wkspower.com>
2024-03-25 16:11:15 +0000

    docker-compose for c7 and c8

---
+++ docker-full-camunda8-shutdown.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run docker-compose with multiple files
+docker-compose -f docker-compose.yaml -f docker-compose.camunda8.yaml -f docker-compose.demo-data-loader.camunda8.yaml -f docker-compose.event-hub.camunda8.yaml -f docker-compose.websocket.yaml -f docker-compose.novu.yaml -f docker-compose.portal.yaml down --remove-orphans -v "$@"
--- docker-full-startup.sh
+++ docker-full-startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run docker-compose with multiple files
-docker-compose -f docker-compose.yaml -f docker-compose.demo-data-loader.yaml -f docker-compose.event-hub.yaml -f docker-compose.websocket.yaml -f docker-compose.novu.yaml -f docker-compose.portal.yaml up -d --build "$@"
+docker-compose -f docker-compose.yaml -f docker-compose.camunda7.yaml -f docker-compose.demo-data-loader.camunda7.yaml -f docker-compose.event-hub.camunda7.yaml -f docker-compose.websocket.yaml -f docker-compose.novu.yaml -f docker-compose.portal.yaml up -d --build "$@"
