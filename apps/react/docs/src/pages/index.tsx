91a04a077dfcfb1f691191ea6cc75f56e8eaf240
francav <victor@wkspower.com>
2024-04-29 10:51:41 +0100

    fixing docs website deployment

--- apps/react/docs/src/pages/index.tsx
+++ apps/react/docs/src/pages/index.tsx
@@ -4,7 +4,6 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import HubspotForm from 'react-hubspot-form';
 
 import styles from './index.module.css';
 
