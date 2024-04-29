25016483215677941b1efa51706b1cdaf2a907fc
Victor Franca <victor@wkspower.com>
2024-04-29 12:17:14 +0100

    bump docusaurus (#233)

---
+++ apps/react/docs/docusaurus.config.ts
@@ -0,0 +1,161 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "WKS Platform",
+  tagline: "Adaptive Case Management Platform",
+  url: "https://docs.wkspower.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "wks-power", // Usually your GitHub org/user name.
+  projectName: "wks-platform-docs", // Usually your repo name.
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          breadcrumbs: false,
+          editUrl: "https://github.com/wkspower/wks-platform-website/tree/main",
+        },
+        blog: {
+          showReadingTime: false,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/wkspower/wks-platform-website/tree/main",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "",
+      logo: {
+        alt: "WKS Platform Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          href: "https://www.wkspower.com/blog",
+          label: "Blog",
+          position: "left",
+        },
+        {
+          type: "doc",
+          docId: "Introduction/intro",
+          position: "left",
+          label: "Docs",
+        },
+        {
+          type: "doc",
+          docId: "Introduction/intro-video",
+          position: "left",
+          label: "Intro Video",
+        },
+        {
+          href: "https://www.wkspower.com/contact",
+          label: "Contact Us",
+          position: "right",
+        },
+        {
+          href: "https://www.wkspower.com/subscribe",
+          label: "Subscribe",
+          position: "right",
+        },
+        {
+          href: "https://wkspower.thinkific.com/courses/wks-platform-acm",
+          label: "In-Company Training",
+          position: "right",
+        },
+        {
+          href: "https://github.com/wkspower/wks-platform",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      // style: "dark",
+      links: [
+        // {
+        //   title: "Docs",
+        //   items: [
+        //     {
+        //       label: "Tutorial",
+        //       to: "/docs/intro",
+        //     },
+        //   ],
+        // },
+        // {
+        //   title: "Community",
+        //   items: [
+        //     {
+        //       label: "Stack Overflow",
+        //       href: "https://stackoverflow.com/questions/tagged/docusaurus",
+        //     },
+        //     {
+        //       label: "Discord",
+        //       href: "https://discordapp.com/invite/docusaurus",
+        //     },
+        //     {
+        //       label: "Twitter",
+        //       href: "https://twitter.com/docusaurus",
+        //     },
+        //   ],
+        // },
+        // {
+        //   title: "More",
+        //   items: [
+        //     {
+        //       label: "Blog",
+        //       to: "/blog",
+        //     },
+        //     {
+        //       label: "GitHub",
+        //       href: "https://github.com/facebook/docusaurus",
+        //     },
+        //   ],
+        // },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} WKS Power.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+if (process.env.PROD === "true") {
+  config.scripts.push({
+    src: "https://www.googletagmanager.com/gtag/js?id=G-DD0FWZXED7",
+    async: true,
+  });
+  config.scripts.push({
+    src: "/js/gtag.js",
+    async: false,
+  });
+}
+
+export default config;
--- apps/react/docs/src/pages/index.tsx
+++ apps/react/docs/src/pages/index.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+  const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            style={{ marginRight: '1rem' }}
             to="/docs/Introduction">
             Documentation
           </Link>
@@ -28,7 +29,7 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
+  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
