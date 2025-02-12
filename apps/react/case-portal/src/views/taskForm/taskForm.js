410e089c985bbcf4739761d8d7835bf15b7326cf
Helder Guilherme Farias <helderfarias@gmail.com>
2025-02-12 19:47:54 -0300

    adding processInstanceId to context of the taskform

--- apps/react/case-portal/src/views/taskForm/taskForm.js
+++ apps/react/case-portal/src/views/taskForm/taskForm.js
@@ -57,6 +57,7 @@ export const TaskForm = ({ open, handleClose, task }) => {
           }
         })
 
+        injectGlobalVariables(apiDataVariables, task)
         setFormComponents(apiDataFormComponents)
         setVariableValues(apiDataVariables)
         setClaimed(task.assignee !== null && task.assignee !== undefined)
@@ -189,3 +190,10 @@ export const TaskForm = ({ open, handleClose, task }) => {
     </div>
   )
 }
+function injectGlobalVariables(apiDataVariables, task) {
+  apiDataVariables['data'] = {
+    ...apiDataVariables.data,
+    processInstanceId: task.processInstanceId,
+  }
+}
+
