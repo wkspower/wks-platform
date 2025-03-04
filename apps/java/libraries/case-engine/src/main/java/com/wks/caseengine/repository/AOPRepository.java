c07bf1204c951c22cdadb214556f6b0cac4db266
Niraj <niraj.gupta9218@gmail.com>
2025-03-04 11:27:49 +0530

    catalyst

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/repository/AOPRepository.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/repository/AOPRepository.java
@@ -26,8 +26,8 @@ public interface AOPRepository extends JpaRepository<AOP, UUID>{
     List<AOP> findAllByAopYearAndPlantFkId(String year, UUID fromString);
 
 
-    @Query(value="select distinct [NormParameters_FK_Id] from BusinessDemand where Plant_FK_Id = :plantId and Year=:year "+
-    " and [NormParameters_FK_Id] not in (select [NormParameters_FK_Id] from [dbo].[AOP] where Plant_FK_Id= :plantId and [NormParameters_FK_Id] is not null and Year=:year) ", nativeQuery=true)
+    @Query(value="select distinct NormParameters_FK_Id from BusinessDemand where Plant_FK_Id = :plantId and Year=:year "+
+    " and NormParameters_FK_Id not in (select NormParameters_FK_Id from AOP where Plant_FK_Id= :plantId and NormParameters_FK_Id is not null and Year=:year) ", nativeQuery=true)
     List<Object[]> getDataBusinessAllData(@Param("plantId") String plantId, @Param("year") String year);
 	
 	
