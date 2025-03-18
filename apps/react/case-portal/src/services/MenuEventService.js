91d3bd8e233c4ec7c62d330bfbe5d34b2a524f42
Rodolfo <rodolfoagomes1987@gmail.com>
2025-03-18 19:38:10 +0000

    New MenuEvenService created to manage menu state after items update

---
+++ apps/react/case-portal/src/services/MenuEventService.js
@@ -0,0 +1,13 @@
+const MENU_UPDATE_EVENT = 'menu-update-event';
+
+export const MenuEventService = {
+  triggerMenuUpdate: () => {
+    const event = new CustomEvent(MENU_UPDATE_EVENT);
+    window.dispatchEvent(event);
+  },
+  
+  subscribeToMenuUpdates: (callback) => {
+    window.addEventListener(MENU_UPDATE_EVENT, callback);
+    return () => window.removeEventListener(MENU_UPDATE_EVENT, callback);
+  }
+};
