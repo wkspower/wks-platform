b9eeae339fee8c6d359e076647a32bd45d26071c
Rodolfo Gomes <rodolfoagomes1987@gmail.com>
2025-04-21 14:10:07 +0100

    issue-335 Typo on the CaseList

--- apps/react/case-portal/src/views/caseList/caseList.js
+++ apps/react/case-portal/src/views/caseList/caseList.js
@@ -167,7 +167,527 @@ export const CaseList = ({ status, caseDefId }) => {
       setCases,
       setFilter,
     )
+  }import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material'
+  import CloseIcon from '@mui/icons-material/Close'
+  import ViewKanbanIcon from '@mui/icons-material/ViewKanban'
+  import ViewListIcon from '@mui/icons-material/ViewList'
+  import RefreshIcon from '@mui/icons-material/Refresh'
+  import { useTheme } from '@mui/material'
+  import Box from '@mui/material/Box'
+  import Button from '@mui/material/Button'
+  import IconButton from '@mui/material/IconButton'
+  import Snackbar from '@mui/material/Snackbar'
+  import TablePagination from '@mui/material/TablePagination'
+  import ToggleButton from '@mui/material/ToggleButton'
+  import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
+  import Tooltip from '@mui/material/Tooltip'
+  import { useSession } from 'SessionStoreContext'
+  import MainCard from 'components/MainCard'
+  import React, {
+    Suspense,
+    createContext,
+    lazy,
+    useContext,
+    useEffect,
+    useState,
+  } from 'react'
+  import { useTranslation } from 'react-i18next'
+  import { CaseService } from '../../services'
+  
+  const DataGrid = lazy(() =>
+    import('@mui/x-data-grid').then((module) => ({ default: module.DataGrid })),
+  )
+  const Kanban = lazy(() =>
+    import('components/Kanban/kanban').then((module) => ({
+      default: module.Kanban,
+    })),
+  )
+  const CaseForm = lazy(() =>
+    import('../caseForm/caseForm').then((module) => ({
+      default: module.CaseForm,
+    })),
+  )
+  const NewCaseForm = lazy(() =>
+    import('../caseForm/newCaseForm').then((module) => ({
+      default: module.NewCaseForm,
+    })),
+  )
+  
+  export const CaseList = ({ status, caseDefId }) => {
+    const PaginationContext = createContext()
+    const { t } = useTranslation()
+    const [stages, setStages] = useState([])
+    const [cases, setCases] = useState([])
+    const [aCase, setACase] = useState(null)
+    const [newCaseDefId, setNewCaseDefId] = useState(null)
+    const [lastCreatedCase, setLastCreatedCase] = useState(null)
+    const [openCaseForm, setOpenCaseForm] = useState(false)
+    const [openNewCaseForm, setOpenNewCaseForm] = useState(false)
+    const [view, setView] = React.useState('list')
+    const [snackOpen, setSnackOpen] = useState(false)
+    const keycloak = useSession()
+    const [caseDefs, setCaseDefs] = useState([])
+    const [fetching, setFetching] = useState(false)
+    const [filter, setFilter] = useState({
+      sort: '',
+      limit: 10,
+      after: '',
+      before: '',
+      cursors: {},
+      hasPrevious: false,
+      hasNext: false,
+    })
+  
+    useEffect(() => {
+      fetchCases(
+        setFetching,
+        keycloak,
+        caseDefId,
+        setStages,
+        status,
+        filter,
+        setCases,
+        setFilter,
+      )
+    }, [caseDefId, status, openNewCaseForm])
+  
+    useEffect(() => {
+      CaseService.getCaseDefinitions(keycloak).then((resp) => {
+        setCaseDefs(resp)
+      })
+    }, [])
+  
+    const handleRefresh = () => {
+      fetchCases(
+        setFetching,
+        keycloak,
+        caseDefId,
+        setStages,
+        status,
+        filter,
+        setCases,
+        setFilter,
+      )
+    }
+  
+    const makeColumns = () => {
+      return [
+        {
+          field: 'businessKey',
+          headerName: t('pages.caselist.datagrid.columns.businesskey'),
+          width: 150,
+        },
+        {
+          field: 'statusDescription',
+          headerName: t('pages.caselist.datagrid.columns.statusdescription'),
+          width: 150,
+        },
+        {
+          field: 'stage',
+          headerName: t('pages.caselist.datagrid.columns.stage'),
+          width: 220,
+        },
+        {
+          field: 'createdAt',
+          headerName: t('pages.caselist.datagrid.columns.createdat'),
+          width: 220,
+        },
+        {
+          field: 'ownerName',
+          headerName: t('pages.caselist.datagrid.columns.caseOwnerName'),
+          width: 150,
+          valueGetter: (value, row) => row?.owner?.name,
+        },
+        {
+          field: 'queueId',
+          headerName: t('pages.caselist.datagrid.columns.queue'),
+          width: 200,
+        },
+        {
+          field: 'action',
+          headerName: '',
+          sortable: false,
+          renderCell: (data) => {
+            const onClick = (e) => {
+              setACase(data.row)
+              e.stopPropagation()
+              setOpenCaseForm(true)
+            }
+  
+            return (
+              <Button onClick={onClick}>
+                {t('pages.caselist.datagrid.action.details')}
+              </Button>
+            )
+          },
+        },
+      ]
+    }
+  
+    const handleCloseCaseForm = () => {
+      setOpenCaseForm(false)
+      fetchCases(
+        setFetching,
+        keycloak,
+        caseDefId,
+        setStages,
+        status,
+        filter,
+        setCases,
+        setFilter,
+      )
+    }
+  
+    const handleCloseNewCaseForm = () => {
+      setOpenNewCaseForm(false)
+      setSnackOpen(true)
+    }
+  
+    const handleNewCaseAction = () => {
+      setLastCreatedCase(null)
+      setNewCaseDefId(caseDefId)
+      setOpenNewCaseForm(true)
+    }
+  
+    const handleChangeView = (event, nextView) => {
+      if (nextView !== null) {
+        setView(nextView)
+      }
+    }
+  
+    const fetchKanbanConfig = () => {
+      return caseDefs.find((o) => o.id === caseDefId).kanbanConfig
+    }
+  
+    const handleCloseSnack = (event, reason) => {
+      if (reason === 'clickaway') {
+        return
+      }
+  
+      setSnackOpen(false)
+    }
+  
+    const snackAction = lastCreatedCase && (
+      <React.Fragment>
+        <Button
+          color='primary'
+          size='small'
+          onClick={() => {
+            setACase({
+              businessKey: lastCreatedCase.businessKey,
+              caseDefinitionId: caseDefId,
+            })
+            setOpenCaseForm(true)
+            handleCloseSnack()
+          }}
+        >
+          {lastCreatedCase.businessKey}
+        </Button>
+        <IconButton
+          size='small'
+          aria-label='close'
+          color='inherit'
+          onClick={handleCloseSnack}
+        >
+          <CloseIcon fontSize='small' />
+        </IconButton>
+      </React.Fragment>
+    )
+  
+    const handlerNextPage = () => {
+      setFetching(true)
+  
+      const next = {
+        sort: filter.sort,
+        limit: filter.limit,
+        after: filter.cursors.after,
+      }
+  
+      CaseService.filterCase(keycloak, caseDefId, status, next)
+        .then((resp) => {
+          const { data, paging } = resp
+  
+          setCases(data)
+          setFilter({
+            ...filter,
+            cursors: paging.cursors,
+            hasPrevious: paging.hasPrevious,
+            hasNext: paging.hasNext,
+          })
+        })
+        .finally(() => {
+          setFetching(false)
+        })
+    }
+  
+    const handlerPriorPage = () => {
+      setFetching(true)
+  
+      const prior = {
+        sort: filter.sort,
+        limit: filter.limit,
+        before: filter.cursors.before,
+      }
+  
+      CaseService.filterCase(keycloak, caseDefId, status, prior)
+        .then((resp) => {
+          const { data, paging } = resp
+  
+          setCases(data)
+          setFilter({
+            ...filter,
+            cursors: paging.cursors,
+            hasPrevious: paging.hasPrevious,
+            hasNext: paging.hasNext,
+          })
+        })
+        .finally(() => {
+          setFetching(false)
+        })
+    }
+  
+    function TablePaginationActions(props) {
+      const theme = useTheme()
+      const filter = useContext(PaginationContext)
+      const { onPageChange } = props
+  
+      const handleBackButtonClick = (event) => {
+        onPageChange(event, 'back')
+      }
+  
+      const handleNextButtonClick = (event) => {
+        onPageChange(event, 'next')
+      }
+  
+      const { hasPrevious, hasNext } = filter
+  
+      return (
+        <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+          <IconButton
+            onClick={handleBackButtonClick}
+            disabled={!hasPrevious}
+            aria-label='previous page'
+          >
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+          </IconButton>
+          <IconButton
+            onClick={handleNextButtonClick}
+            disabled={!hasNext}
+            aria-label='next page'
+          >
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </IconButton>
+        </Box>
+      )
+    }
+  
+    const CustomPagination = () => {
+      return (
+        <PaginationContext.Provider value={filter}>
+          <TablePagination
+            component='div'
+            count={-1}
+            page={0}
+            labelRowsPerPage={
+              <div style={{ paddingTop: 15 }}>Rows per page:</div>
+            }
+            rowsPerPage={filter.limit}
+            rowsPerPageOptions={[5, 10, 25, 50]}
+            getItemAriaLabel={() => ''}
+            labelDisplayedRows={() => ''}
+            onPageChange={(e, type) => {
+              const action = {
+                next: handlerNextPage,
+                back: handlerPriorPage,
+              }
+              action[type]()
+            }}
+            onRowsPerPageChange={(e) => {
+              setFetching(true)
+  
+              CaseService.filterCase(keycloak, caseDefId, status, {
+                limit: e.target.value,
+              })
+                .then((resp) => {
+                  const { data, paging } = resp
+  
+                  setCases(data)
+                  setFilter({
+                    ...filter,
+                    limit: e.target.value,
+                    cursors: paging.cursors,
+                    hasPrevious: paging.hasPrevious,
+                    hasNext: paging.hasNext,
+                  })
+                })
+                .finally(() => {
+                  setFetching(false)
+                })
+            }}
+            SelectProps={{
+              inputProps: {
+                'aria-label': 'rows per page',
+              },
+              native: true,
+            }}
+            ActionsComponent={TablePaginationActions}
+          />
+        </PaginationContext.Provider>
+      )
+    }
+  
+    return (
+      <div style={{ height: 650, width: '100%' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 2 }}>
+          <Box>
+            {caseDefId && (
+              <Button
+                id='basic-button'
+                onClick={handleNewCaseAction}
+                variant='contained'
+              >
+                {t('pages.caselist.action.newcase')}
+              </Button>
+            )}
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            {caseDefId && (
+              <ToggleButtonGroup
+                orientation='horizontal'
+                value={view}
+                exclusive
+                onChange={handleChangeView}
+                sx={{ mr: 2 }}
+              >
+                <ToggleButton value='list' aria-label='list'>
+                  <ViewListIcon />
+                </ToggleButton>
+                <ToggleButton value='kanban' aria-label='kanban'>
+                  <ViewKanbanIcon />
+                </ToggleButton>
+              </ToggleButtonGroup>
+            )}
+            <Tooltip title={t('pages.caselist.action.refresh')}>
+              <IconButton
+                onClick={handleRefresh}
+                color='primary'
+                disabled={fetching}
+              >
+                <RefreshIcon />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        </Box>
+  
+        <MainCard sx={{ mt: 2 }} content={false}>
+          <Box>
+            {view === 'list' && (
+              <div>
+                <Suspense fallback={<div>Loading...</div>}>
+                  <DataGrid
+                    sx={{
+                      height: 500,
+                      width: '100%',
+                      backgroundColor: '#ffffff',
+                      mt: 1,
+                    }}
+                    rows={cases}
+                    columns={makeColumns()}
+                    getRowId={(row) => row.busi100nessKey}
+                    loading={fetching}
+                    components={{ Pagination: CustomPagination }}
+                  />
+                </Suspense>
+              </div>
+            )}
+            {view === 'kanban' && (
+              <Suspense fallback={<div>Loading...</div>}>
+                <Kanban
+                  stages={stages}
+                  cases={cases}
+                  caseDefId={caseDefId}
+                  kanbanConfig={fetchKanbanConfig()}
+                  setACase={setACase}
+                  setOpenCaseForm={setOpenCaseForm}
+                />
+              </Suspense>
+            )}
+          </Box>
+        </MainCard>
+  
+        <br />
+  
+        {openCaseForm && (
+          <CaseForm
+            aCase={aCase}
+            handleClose={handleCloseCaseForm}
+            open={openCaseForm}
+            keycloak={keycloak}
+          />
+        )}
+  
+        {openNewCaseForm && (
+          <NewCaseForm
+            handleClose={handleCloseNewCaseForm}
+            open={openNewCaseForm}
+            caseDefId={newCaseDefId}
+            setLastCreatedCase={setLastCreatedCase}
+          />
+        )}
+  
+        {lastCreatedCase && (
+          <Snackbar
+            open={snackOpen}
+            autoHideDuration={6000}
+            message='Case created'
+            onClose={handleCloseSnack}
+            action={snackAction}
+          />
+        )}
+      </div>
+    )
   }
+  function fetchCases(
+    setFetching,
+    keycloak,
+    caseDefId,
+    setStages,
+    status,
+    filter,
+    setCases,
+    setFilter,
+  ) {
+    setFetching(true)
+  
+    CaseService.getCaseDefinitionsById(keycloak, caseDefId)
+      .then((resp) => {
+        resp.stages.sort((a, b) => a.index - b.index).map((o) => o.name)
+        setStages(resp.stages)
+        return CaseService.filterCase(keycloak, caseDefId, status, filter)
+      })
+      .then((resp) => {
+        const { data, paging } = resp
+        setCases(data)
+        setFilter({
+          ...filter,
+          cursors: paging.cursors,
+          hasPrevious: paging.hasPrevious,
+          hasNext: paging.hasNext,
+        })
+      })
+      .finally(() => {
+        setFetching(false)
+      })
+  }
+  
 
   const handleCloseNewCaseForm = () => {
     setOpenNewCaseForm(false)
@@ -331,7 +851,7 @@ export const CaseList = ({ status, caseDefId }) => {
             <div style={{ paddingTop: 15 }}>Rows per page:</div>
           }
           rowsPerPage={filter.limit}
-          rowsPerPageOptions={[2,5, 10, 25, 50]}
+          rowsPerPageOptions={[5, 10, 25, 50]}
           getItemAriaLabel={() => ''}
           labelDisplayedRows={() => ''}
           onPageChange={(e, type) => {
