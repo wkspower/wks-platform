53ede228bdad5c0d80f0e0f431de52219360e298
Niraj <niraj.gupta9218@gmail.com>
2025-05-02 16:10:57 +0530

    get all grades

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/NormParametersServiceImpl.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/NormParametersServiceImpl.java
@@ -1,6 +1,7 @@
  package com.wks.caseengine.service;
 
- import java.util.List;
+ import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,8 +45,24 @@ import jakarta.persistence.Query;
 			String sql = "SELECT * FROM " + viewName + " WHERE Plant_FK_Id = :plantFkId";
 	        Query query = entityManager.createNativeQuery(sql);
 	        query.setParameter("plantFkId", plantFkId);
-
-	        return query.getResultList();
+	        List<NormParameters> normParametersList=new ArrayList<>();
+	        List<Object[]> obj= query.getResultList();
+	        
+	        for(Object[] row : obj) {
+	        	NormParameters normParameters = new NormParameters();
+	        	normParameters.setId(row[0] != null ? UUID.fromString(row[0].toString()) : null);
+	        	normParameters.setName(row[1] != null ? row[1].toString() : "");
+	        	normParameters.setDisplayName(row[2] != null ? row[2].toString() : "");
+	        	normParameters.setNormParameterTypeFkId(row[8] != null ? UUID.fromString(row[8].toString()) : null);
+	        	normParameters.setPlantFkId(row[9] != null ? UUID.fromString(row[9].toString()) : null);
+	        	normParameters.setNormTypeFKId(row[10] != null ? Integer.parseInt(row[10].toString()) : null);
+	        	normParameters.setDisplayOrder(row[12] != null ? Integer.parseInt(row[12].toString()) : null);
+	        	normParameters.setIsEditable(row[13] != null ? Boolean.parseBoolean(row[13].toString()) : null);
+	        	normParameters.setIsVisible(row[14] != null ? Boolean.parseBoolean(row[14].toString()) : null);
+	        	normParametersList.add(normParameters);
+	        	
+	        }
+	        return normParametersList;
 		} catch (IllegalArgumentException e) {
 			throw new RestInvalidArgumentException("Invalid UUID format for Plant ID", e);
 		} catch (Exception ex) {
