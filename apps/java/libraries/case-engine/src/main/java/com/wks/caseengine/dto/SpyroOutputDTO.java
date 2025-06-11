b7b138f2bf49580debdab57ddeaa98a9654b081e
nirajgupta9218 <niraj.gupta9218@gmail.com>
2025-06-11 16:58:29 +0530

    spyro output get and update api

---
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/dto/SpyroOutputDTO.java
@@ -0,0 +1,43 @@
+package com.wks.caseengine.dto;
+
+
+
+import org.springframework.context.annotation.Configuration;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Configuration
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class SpyroOutputDTO {
+	
+	private String VerticalFKId;
+    private String PlantFKId;
+    private String NormParameterFKID;
+    private String Particulars;
+    private String NormParameterTypeName;
+    private String NormParameterTypeFKID;
+    private String Type;
+    private String UOM;
+    private String AuditYear;
+    private String Remarks;
+
+    private Double Jan;
+    private Double Feb;
+    private Double Mar;
+    private Double Apr;
+    private Double May;
+    private Double Jun;
+    private Double Jul;
+    private Double Aug;
+    private Double Sep;
+    private Double Oct;
+    private Double Nov;
+    private Double Dec;
+
+}
