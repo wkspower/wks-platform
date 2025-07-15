ff0c02326b682ae013d59e0e408d89d51fe92fac
nirajgupta9218 <niraj.gupta9218@gmail.com>
2025-07-15 16:49:55 +0530

    Decoking activities in cracker

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/BasisReportService.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/BasisReportService.java
@@ -1,9 +1,12 @@
 package com.wks.caseengine.service;
 
+import java.util.Date;
+
 import com.wks.caseengine.message.vm.AOPMessageVM;
 
 public interface BasisReportService {
 	
-	public AOPMessageVM getNormBasisReport( String plantId, String aopYear, String type);
+	public AOPMessageVM getNormBasisReport( String plantId, String aopYear, String type,Date periodFrom, Date periodTo);
+	public AOPMessageVM getNormBasisReportForPE( String plantId, String aopYear, String type,Date periodFrom, Date periodTo);
 
 }
