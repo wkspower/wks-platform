ba2e375ce3b09d649f6b39bdf69755f728d71074
nirajgupta9218 <niraj.gupta9218@gmail.com>
2025-07-20 22:47:01 +0530

    Slowdown configuration

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/repository/NormAttributeTransactionsRepository.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/repository/NormAttributeTransactionsRepository.java
@@ -174,10 +174,10 @@ public interface NormAttributeTransactionsRepository extends JpaRepository<NormA
 	Optional<NormAttributeTransactions> findByNormParameterFKId(UUID normParameterFKId);
 	
 	@Query(value = "SELECT * FROM NormAttributeTransactions " +
-            "WHERE (PlantMaintenanceTransaction_FK_Id = :maintenanceId OR PlantMaintenanceTransaction_FK_Id IS NULL)" +
+            "WHERE PlantMaintenanceTransaction_FK_Id IS NOT NULL " +
             "AND NormParameter_FK_Id = :normParameterFKId " +
             "AND AuditYear = :auditYear AND AOPMonth = :month", nativeQuery = true)
-	NormAttributeTransactions findByMaintenanceIdAndNormParameterFKIdAndAuditYear(
+	List<NormAttributeTransactions> findByMaintenanceIdAndNormParameterFKIdAndAuditYear(
 	     @Param("maintenanceId") UUID maintenanceId,
 	     @Param("normParameterFKId") UUID normParameterFKId,
 	     @Param("auditYear") String auditYear,
