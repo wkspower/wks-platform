b73d72a3dbf8a97525ed231866ec121d3a3c315f
nirajgupta9218 <niraj.gupta9218@gmail.com>
2025-08-29 09:50:36 +0530

    Grade in shutdown norms

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/repository/GradeShutdownNormsValueRepository.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/repository/GradeShutdownNormsValueRepository.java
@@ -14,4 +14,18 @@ import com.wks.caseengine.entity.ShutdownNormsValue;
 @Repository
 public interface GradeShutdownNormsValueRepository extends JpaRepository<GradeShutdownNormsValue,UUID>{
 	
+	@Query(value = "SELECT TOP 1 Id FROM GradeShutdownNormsValue " +
+            "WHERE Plant_FK_Id = :plantId " +
+            "AND Site_FK_Id = :siteId " +
+            "AND Vertical_FK_Id = :verticalId " +
+            "AND Material_FK_Id = :materialId " +
+            "AND FinancialYear = :financialYear", 
+    nativeQuery = true)
+	UUID findIdByFilters(@Param("plantId") UUID plantId,
+                  @Param("siteId") UUID siteId,
+                  @Param("verticalId") UUID verticalId,
+                  @Param("materialId") UUID materialId,
+                  @Param("financialYear") String financialYear);
+
+	
 }
