7f4a283013ed9b9198506753e7152b082d5403f9
Shivanand Raut <shivanandraut007@gmail.com>
2025-09-17 17:15:04 +0530

    validation of date for cracker datepicker

--- apps/react/case-portal/src/components/kendo-data-tables/Utilities-Kendo/DatePicker.js
+++ apps/react/case-portal/src/components/kendo-data-tables/Utilities-Kendo/DatePicker.js
@@ -13,12 +13,29 @@ const DateOnlyPicker = ({ dataItem, field, onChange }) => {
     })
   }
 
+  // Get AOP year from localStorage
+  const fyString = localStorage.getItem('year')
+  let fyMin = null
+  let fyMax = null
+
+  if (fyString) {
+    const [start, end] = fyString.split('-')
+    const startYear = parseInt(start, 10)
+    let endYear = parseInt(end, 10)
+    if (end.length === 2) endYear += 2000
+
+    fyMin = new Date(startYear, 3, 1) // April 1
+    fyMax = new Date(endYear, 2, 31)  // March 31
+  }
+
   return (
     <td>
       <DatePicker
         value={currentDate}
         format='dd-MM-yyyy'
         onChange={handleChange}
+        min={fyMin}
+        max={fyMax}
         width='100%'
         size='small'
         style={{
