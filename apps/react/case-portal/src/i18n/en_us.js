16cbe5628adcf7b81ee821138583ff0ac4aa7532
Shivanand Raut <shivanandraut007@gmail.com>
2025-12-01 20:18:01 +0530

    label name chnage

--- apps/react/case-portal/src/i18n/en_us.js
+++ apps/react/case-portal/src/i18n/en_us.js
@@ -34,7 +34,7 @@ const defs = {
     spyroOutput: 'Optimizer Output',
     spyroMenu: 'Optimizer Menu',
     decokingActivities: 'Maintenance Details (Furnace & SAD plan)',
-    maintenanceDetails: 'Net Production Hour',
+    maintenanceDetails: 'Net Production Hours',
     consumptionNorms: 'Overall AOP Consumption (Norm/Quantity)',
     productionNorms: 'Month wise Production plan',
     catalystSelectivity: 'Production & Norms Basis',
@@ -232,7 +232,7 @@ const defs = {
       title: 'Production / Norms Plan',
       subPages: {
         productMCUVal: 'Production Target',
-        maintenanceDetails: 'Net Production Hour',
+        maintenanceDetails: 'Net Production Hours',
         consumptionNorms: 'Overall AOP Consumption (Norm/Quantity)',
         productionNorms: 'Month wise Production plan',
         catalystSelectivity: 'Production & Norms Basis',
