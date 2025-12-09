baf59d2fe31b70e578a52fde67c9f7797a21e7c3
nirajgupta9218 <niraj.gupta9218@gmail.com>
2025-12-09 17:46:24 +0530

    Packing consumable

--- apps/java/services/case-engine-rest-api/src/main/java/com/wks/caseengine/rest/server/PackagingConsumablesController.java
+++ apps/java/services/case-engine-rest-api/src/main/java/com/wks/caseengine/rest/server/PackagingConsumablesController.java
@@ -1,10 +1,16 @@
 package com.wks.caseengine.rest.server;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.wks.caseengine.dto.ConfigurationDTO;
 import com.wks.caseengine.message.vm.AOPMessageVM;
 import com.wks.caseengine.service.PackagingConsumablesService;
 
@@ -18,6 +24,11 @@ public class PackagingConsumablesController {
 	@GetMapping(value="/packaging-consumables")
 	public AOPMessageVM getPackagingConsumables(@RequestParam String plantId,@RequestParam String year){
 		 return  packagingConsumablesService.getPackagingConsumables(plantId,year);
+	}
+	
+	@PostMapping(value="/packaging-consumables")
+	public AOPMessageVM savePackagingConsumables(@RequestParam String year,@RequestParam String plantFKId, @RequestBody List<ConfigurationDTO> configurationDTOList) {
+		return 	packagingConsumablesService.savePackagingConsumables(year,plantFKId,configurationDTOList);
 	}
 	
 }
