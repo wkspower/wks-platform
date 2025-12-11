332415be344a83d12a44c6bbd39a9e598f1a06d3
nirajgupta9218 <niraj.gupta9218@gmail.com>
2025-12-11 22:28:28 +0530

    ShutdownHistoryConfig

---
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/dto/ShutdownHistoryConfigDTO.java
@@ -0,0 +1,23 @@
+package com.wks.caseengine.dto;
+
+import lombok.*;
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ShutdownHistoryConfigDTO {
+
+    private UUID id;
+    private Integer month;
+    private String year;
+    private String aopYear;
+    private String remark;
+    private Date createdOn;
+    private Date modifiedOn;
+    private String modifiedBy;
+
+}
---
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/repository/ShutdownHistoryConfigRepository.java
@@ -0,0 +1,22 @@
+package com.wks.caseengine.repository;
+
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.wks.caseengine.entity.ReliabilityRecords;
+import com.wks.caseengine.entity.ShutdownHistoryConfig;
+
+@Repository
+public interface ShutdownHistoryConfigRepository extends JpaRepository<ShutdownHistoryConfig,UUID>{
+	 @Query(value = "SELECT * FROM Elastomer_ShutdownHistoryConfig WHERE AOPYear = :aopYear", 
+	           nativeQuery = true)
+	    List<ShutdownHistoryConfig> findByAopYear(@Param("aopYear") String aopYear);
+	
+	
+}
