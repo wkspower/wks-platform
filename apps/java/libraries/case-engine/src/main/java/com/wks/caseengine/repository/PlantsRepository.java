217ab6969c9233a3d070a751c88334ce3bd0a960
nirajgupta9218 <niraj.gupta9218@gmail.com>
2026-01-21 16:45:47 +0530

    PTA Slowdown and Shutdown months

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/repository/PlantsRepository.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/repository/PlantsRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.wks.caseengine.dto.PlantConsumpProjection;
 import com.wks.caseengine.entity.Plants;
 
 @Repository
