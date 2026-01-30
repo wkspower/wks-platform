c3fd49aeb61dbec35c9dcbf187a5ad3961cf6d3a
karan.kapoor <karan.kapoor@vetology.net>
2026-01-30 16:16:55 +0530

    tcs workflow debug, changes related to audit trail

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/tcs/service/TCSWorkFlowService.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/tcs/service/TCSWorkFlowService.java
@@ -8,13 +8,13 @@ public interface TCSWorkFlowService {
     
     void completePlantSubmissionTask(String plantName, String siteId, PlantSubmissionAuditTrailDTO plantSubmissionAuditTrailDTO, String finacialYear);
 
-    void ebsApproval(String plantName, String siteId, PlantSubmissionAuditTrailDTO plantSubmissionAuditTrailDTO, String finacialYear);
+    void ebsApproval(String siteId, PlantSubmissionAuditTrailDTO plantSubmissionAuditTrailDTO, String finacialYear);
 
     void ebsApproveReject(String plantName, String siteId, boolean approvalStatus, PlantSubmissionAuditTrailDTO plantSubmissionAuditTrailDTO, String finacialYear);
 
     void startProcess(String verticalId, String siteId, String finacialYear);
 
-    void CTSApproval(String plantName, String siteId, PlantSubmissionAuditTrailDTO plantSubmissionAuditTrailDTO, String finacialYear);
+    void CTSApproval(String siteId, PlantSubmissionAuditTrailDTO plantSubmissionAuditTrailDTO, String finacialYear);
 
     List<PlantSubmissionAuditTrailDTO> getSubmissionAuditTrail(String plantName, String siteId, String verticalId, String type);
 
