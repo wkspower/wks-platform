c7d71bf4a1cefd1b115a48a005324e6a233a74cd
Shivanand Raut <shivanandraut007@gmail.com>
2026-02-10 11:02:27 +0530

    VCM condition of vertical

--- apps/react/case-portal/src/components/kendo-data-tables/NormalOpNorms.js
+++ apps/react/case-portal/src/components/kendo-data-tables/NormalOpNorms.js
@@ -85,7 +85,7 @@ const NormalOpNormsScreen = () => {
 
   const isPEPP = lowerVertName === 'pe' || lowerVertName === 'pp'
   const isPET = lowerVertName === 'pet'
-
+  const IS_VCM_VERTICAL = lowerVertName === 'vcm'
   const keycloak = useSession()
   // const READ_ONLY = getRoleName(keycloak)
   const READ_ONLY = getRoleName(keycloak, IS_OLD_YEAR)
