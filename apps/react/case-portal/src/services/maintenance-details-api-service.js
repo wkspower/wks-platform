0b1be617c12669f42eaf968ec004b48919816a14
Shivanand Raut <shivanandraut007@gmail.com>
2026-02-16 12:28:47 +0530

    Type od SD days Api configuration

--- apps/react/case-portal/src/services/maintenance-details-api-service.js
+++ apps/react/case-portal/src/services/maintenance-details-api-service.js
@@ -16,6 +16,7 @@ export const MaintenanceDetailsApiService = {
   saveSlowdownConfig,
   getSlowdownConfig,
   getCrackerDownsteamShutdownDMD,
+  getSdDaysValues,
 }
 
 async function getCrackerMaintenanceData(keycloak, PLANT_ID, AOP_YEAR) {
@@ -336,6 +337,22 @@ async function getSlowdownConfig(keycloak, PLANT_ID, AOP_YEAR) {
 }
 async function getCrackerDownsteamShutdownDMD(keycloak, PLANT_ID, AOP_YEAR) {
   const url = `${Config.CaseEngineUrl}/task/maintenance-other-plants?plantId=${PLANT_ID}&year=${AOP_YEAR}`
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${keycloak.token}`,
+  }
+  try {
+    const resp = await fetch(url, { method: 'GET', headers })
+    return json(keycloak, resp)
+  } catch (e) {
+    console.log(e)
+    return await Promise.reject(e)
+  }
+}
+
+async function getSdDaysValues(keycloak, PLANT_ID, AOP_YEAR) {
+  const url = `${Config.CaseEngineUrl}/task/type-of-sd?plantId=${PLANT_ID}&year=${AOP_YEAR}`
   const headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
